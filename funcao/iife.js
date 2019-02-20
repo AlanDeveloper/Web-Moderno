a0bf0707ef2ea20ec4a7dab55de694ac545b1916
AlanDeveloper <alanssantos32@gmail.com>
2019-02-20 10:14:06 -0300

    Add files via upload

---
+++ funcao/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> Immediately invoked Function Expression
+
+(function() {
+    console.log('Será executado na hora!');
+    console.log('Foge do escopo mais abrangente!');
+})();
+
+// Qualquer coisa criada dentro da função, só é acessível dentro da mesma
