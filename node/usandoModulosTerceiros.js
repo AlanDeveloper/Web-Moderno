d98d96c6cd71ad90b92464f0f87b4b26ffdb1e00
Alan Santos <alanssantos32@gmail.com>
2019-03-07 10:37:34 -0300

    Add files via upload

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+setInterval(() => console.log(_.random(1, 1000)), 2000);
+
+// Nodemon
+
+// linux -> sudo npm i -g nodemon
+// windows-> npm i -g nodemon
+
+// Teste dinâmico de js
+// "cd node"
+// "nomemon script.js"
